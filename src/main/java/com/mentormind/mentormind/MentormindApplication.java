dde16e579ad0bc1b84f8ab22d81d13f52b9258c4
israel7104 <israel7104@gmail.com>
2025-11-19 19:54:09 +0100

    Comienzo del backend

---
+++ src/main/java/com/mentormind/mentormind/MentormindApplication.java
@@ -0,0 +1,13 @@
+package com.mentormind.mentormind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentormindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentormindApplication.class, args);
+	}
+
+}
