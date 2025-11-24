86f24a2a16d49a45e95dff3794edc0b477d537e1
Juanito_DEV <juan_mm_jose@hotmail.com>
2025-11-24 12:29:01 +0100

    fix: update database configuration and enable global access middleware
    - Corrected MySQL datasource settings in application.properties
    - Added required MySQL connector dependency for Spring Data JPA
    - Implemented temporary SecurityConfig to allow unrestricted access to all routes during development
    - Ensured Hibernate can create and manage entity tables correctly

--- src/main/java/com/mentormind/mentormind/entities/UserEntity.java
+++ src/main/java/com/mentormind/mentormind/entities/UserEntity.java
@@ -5,24 +5,27 @@
     import lombok.Getter;
     import lombok.NoArgsConstructor;
     import lombok.Setter;
-    import org.springframework.stereotype.Service;
 
 
     @Entity
     @Getter @Setter
     @AllArgsConstructor
     @NoArgsConstructor
+    @Table(name = "users")
     public class UserEntity{
+
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
+
         private String username;
         private String email;
         private String password_hash;
-        private Integer rank;
+        private Integer score;
         private Integer medal;
         private Boolean admin;
         //Is STUDENT or TEACHER
+
         @Enumerated(EnumType.STRING)
         private Teacher rol;
     }
